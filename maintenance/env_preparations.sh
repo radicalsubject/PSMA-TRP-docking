5a44ea0efc829f3eb232745c14704e8b3f459068
radicalsubject <olegteravolt@gmail.com>
2021-10-01 16:41:54 +0300

    Update env_preparations.sh

--- maintenance/env_preparations.sh
+++ maintenance/env_preparations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -i
 source conda activate && \
-    conda env create -f ../env.yml || \
-    conda env update -f ../env.yml && \
+    conda env create -f ./env.yml || \
+    conda env update -f ./env.yml && \
     conda activate vina 
