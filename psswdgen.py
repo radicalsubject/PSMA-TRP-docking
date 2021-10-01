b75b3d525a681570637168486a62edc98307d512
radicalsubject <olegteravolt@gmail.com>
2021-10-01 16:05:50 +0300

    psswdgen

--- psswdgen.py
+++ psswdgen.py
@@ -7,10 +7,9 @@ if len(sys.argv)==2:
 
 else:
     password = input("type password: ")
-# password = input()
 hashed_argon2 = passwd(password)
 hashed_sha1 = passwd(password, 'sha1')
 # print(hashed_argon2)
 print(hashed_sha1)
-# passwd_check(hashed_argon2, password)
-# passwd_check(hashed_sha1, password)
+passwd_check(hashed_argon2, password)
+passwd_check(hashed_sha1, password)
