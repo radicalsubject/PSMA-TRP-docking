dc842318f4c0438fb1deaf5d7e495c9973b309a7
radicalsubject <olegteravolt@gmail.com>
2021-10-01 14:52:49 +0300

    Update run_scipynotebook.sh

--- run_scipynotebook.sh
+++ run_scipynotebook.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -i
 
-if $1:
+if [ $# -eq 0 ] 
+then
+    read -e -p "set password: " password
+    echo $password
+else
     docker run -p 40001:8888 -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$1 --allow-root
-
+fi
 #'sha1:1f44b533bdd1:34e68851b0bb05a774ed26b60f839a045f3ab022'
