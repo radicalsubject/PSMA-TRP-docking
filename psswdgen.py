feb7deb4e914c858c1fb39c0c585062242808883
radicalsubject <olegteravolt@gmail.com>
2021-10-01 12:46:47 +0300

    Update psswdgen.py

--- psswdgen.py
+++ psswdgen.py
@@ -1,7 +1,7 @@
 from notebook.auth import passwd
 from notebook.auth.security import passwd_check
-# password = input("type password: ")
-password = input()
+password = input("type password: ")
+# password = input()
 hashed_argon2 = passwd(password)
 hashed_sha1 = passwd(password, 'sha1')
 # print(hashed_argon2)
