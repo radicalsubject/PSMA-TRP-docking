71e3a777c0c05733b4743f2691d8686ccdad566f
radicalsubject <olegteravolt@gmail.com>
2021-10-01 16:38:35 +0300

    Update run_scipynotebook.sh

--- maintenance/run_scipynotebook.sh
+++ maintenance/run_scipynotebook.sh
@@ -7,7 +7,7 @@ then
     echo $password
     echo $hashcode
     docker run -p 40001:8888 --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$hashcode --allow-root
-    docker exec -ti notebook sh -c "bash ~/work/maintenance/env_preparations.sh"
+    # docker exec -ti notebook sh -c "bash ~/work/maintenance/env_preparations.sh"
 
 else
     docker run -p 40001:8888 --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$1 --allow-root
