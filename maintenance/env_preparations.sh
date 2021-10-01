bf57558f0af10dee5ef25af51847b79bef5feaa8
radicalsubject <olegteravolt@gmail.com>
2021-10-01 16:31:26 +0300

    environments

--- maintenance/env_preparations.sh
+++ maintenance/env_preparations.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -i
-source /home/ofedorov/anaconda3/etc/profile.d/conda.sh && \
-    conda activate && \
-    conda env create -f env.yml || \
-    conda env update -f env.yml && \
+source conda activate && \
+    conda env create -f ../env.yml || \
+    conda env update -f ../env.yml && \
     conda activate vina 
