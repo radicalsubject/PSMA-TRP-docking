a36ede33f23c5d1d9330fd35c6d78d62f22b7b77
radicalsubject <olegteravolt@gmail.com>
2021-10-02 23:05:58 +0300

    Update run_scipynotebook.sh

--- maintenance/run_scipynotebook.sh
+++ maintenance/run_scipynotebook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -i
 
 grepresponse=$(docker ps -a | grep notebook | grep -oE '[^ ]+$')
-
+echo $grepresponse
+echo [ $grepresponse -eq '' ]
 if [ $grepresponse -eq '' ]
 then
     docker start notebook
@@ -10,8 +11,8 @@ else
     then
         read -p "set password: " password
         hashcode=$(python ./maintenance/psswdgen.py $password)
-        echo $password
-        echo $hashcode
+        # echo $password
+        # echo $hashcode
         docker run -p 40001:8888 --rm --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$hashcode --allow-root
         # use --rm switch i.e., container gets destroyed automatically as soon as it is stopped
         docker exec -d notebook sh -c "bash /home/asya/work/maintenance/env_preparations.sh"
