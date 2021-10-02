e4363742541dd9557e3d73d95e1be6293888d8e0
radicalsubject <olegteravolt@gmail.com>
2021-10-03 00:51:12 +0300

    Update env_preparations.sh

--- maintenance/env_preparations.sh
+++ maintenance/env_preparations.sh
@@ -4,5 +4,5 @@ source conda activate && \
     conda env update -f ./work/env.yml && \
     conda activate vina
     read -p "set password: " password 
-    hashcode=$(python ./maintenance/psswdgen.py $password)
+    hashcode=$(python work/maintenance/psswdgen.py $password)
     jupyter-notebook --NotebookApp.password=$hashcode --allow-root
