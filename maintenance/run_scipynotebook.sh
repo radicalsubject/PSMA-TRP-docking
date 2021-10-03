ff96abcdc2fe2f6d663a127f064eeaa644be7b49
radicalsubject <olegteravolt@gmail.com>
2021-10-03 11:32:27 +0300

    Update run_scipynotebook.sh

--- maintenance/run_scipynotebook.sh
+++ maintenance/run_scipynotebook.sh
@@ -16,8 +16,10 @@ else
         hashcode=$(python ./maintenance/psswdgen.py $password)
         # echo $password
         # echo $hashcode
+        echo "launching docker run"
         docker run -d -p 40001:8888 --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$hashcode --allow-root
         # use --rm switch i.e., container gets destroyed automatically as soon as it is stopped
+        echo "creating vina env"
         docker exec -d notebook sh -c "bash /home/asya/work/maintenance/env_preparations.sh"
 
     else
