c55f43dd20845113e53b38a1f96465f9ed5bba0a
radicalsubject <olegteravolt@gmail.com>
2021-10-03 11:20:05 +0300

    Update run_vina_notebook.sh

--- maintenance/run_vina_notebook.sh
+++ maintenance/run_vina_notebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -i
 read -p "set password: " password 
 hashcode=$(python ./work/maintenance/psswdgen.py $password)
-jupyter-notebook -p 40002:8889 --NotebookApp.password=$hashcode --allow-root
+jupyter-notebook --port=8889 --NotebookApp.password=$hashcode --allow-root
