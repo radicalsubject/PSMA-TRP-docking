5260e7d1f7e8579a04223949aeead3d768bee7de
radicalsubject <olegteravolt@gmail.com>
2021-10-03 12:39:01 +0300

    summary

--- maintenance/env_preparations.sh
+++ maintenance/env_preparations.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -i
+#!/bin/bash -
+
 conda env create -f ./work/env.yml || \
     conda env update -f ./work/env.yml
 
--- maintenance/run_vina_notebook.sh
+++ maintenance/run_vina_notebook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -i
-read -p "set password: " password 
 
+read -p "set password: " password 
 hashcode=$(python ./work/maintenance/psswdgen.py $password)
 jupyter-notebook --port=8889 --NotebookApp.password=$hashcode --allow-root
