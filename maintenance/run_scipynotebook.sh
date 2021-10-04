a0acb52a353030c1b6087a81f2a92d5034c2dc43
radicalsubject <olegteravolt@gmail.com>
2021-10-04 12:29:24 +0300

    Update run_scipynotebook.sh

--- maintenance/run_scipynotebook.sh
+++ maintenance/run_scipynotebook.sh
@@ -18,16 +18,18 @@ else
         # echo $password
         # echo $hashcode
         echo "launching docker run"
-        docker run -ti -p 40001:8888 --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$hashcode --allow-root 
+        docker run -d -p 40001:8888 --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$hashcode --allow-root 
         #&> /dev/null
         # use --rm switch i.e., container gets destroyed automatically as soon as it is stopped
+        sleep 20
         echo "creating vina env"
         docker exec -ti notebook sh -c "bash /home/asya/work/maintenance/env_preparations.sh"
 
     else
-        docker run -ti -p 40001:8888 --rm --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$1 --allow-root 
+        docker run -d -p 40001:8888 --rm --name notebook -e NB_USER=asya -w /home/asya -e CHOWN_HOME=yes -v "${PWD}":/home/asya/work -e GRANT_SUDO=yes --user root jupyter/scipy-notebook jupyter-notebook --NotebookApp.password=$1 --allow-root 
         #&> /dev/null
-        # use --rm switch i.e., container gets destroyed automatically as soon as it is stopped
+        # use --rm switch i.e., container gets destroyed automdcally as soon as it is stopped
+        sleep 20
         docker exec -ti notebook sh -c "bash /home/asya/work/maintenance/env_preparations.sh"
     fi
 fi
