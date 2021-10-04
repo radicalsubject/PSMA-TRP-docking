2a81c3f3fa28937aaa9350a39a16eae1415b9fac
radicalsubject <olegteravolt@gmail.com>
2021-10-04 12:44:08 +0300

    Update psswdgen.py

--- maintenance/psswdgen.py
+++ maintenance/psswdgen.py
@@ -5,9 +5,7 @@ from notebook.auth.security import passwd_check
 if len(sys.argv)==2:
     password = sys.argv[1]
 else:
-    password = input("type password: ")
-
-
+    password = input('type password:')
 hashed_argon2 = passwd(password)
 hashed_sha1 = passwd(password, 'sha1')
 # print(hashed_argon2)
