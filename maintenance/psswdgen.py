80e1b86bce257411ada9beaf6b7588319df2a01e
radicalsubject <olegteravolt@gmail.com>
2021-10-04 12:38:04 +0300

    Update psswdgen.py

--- maintenance/psswdgen.py
+++ maintenance/psswdgen.py
@@ -11,5 +11,6 @@ hashed_argon2 = passwd(password)
 hashed_sha1 = passwd(password, 'sha1')
 # print(hashed_argon2)
 print(hashed_sha1)
+
 passwd_check(hashed_argon2, password)
 passwd_check(hashed_sha1, password)
